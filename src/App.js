49cec7fac0345dd7ad8c91c8c615d24b6f1de98c
Aishu2604 <aishwary.nigam04@gmail.com>
2022-09-27 17:34:06 +0530

    Refs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import AddUser from "./components/User/AddUser";
 import UserList from "./components/User/UserList";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
-  const addUserHandler = (uName, uAge) => {
+  const addUserHandler = (uName, uAge, uClg) => {
     setUsersList((prevUsersList) => {
       return [
         ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
+        { name: uName, age: uAge, college: uClg, id: Math.random().toString() },
       ];
     });
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UserList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
